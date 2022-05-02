0a1e5fbce5a6681fb783a68629f46a5e0c70a22b
aliceduren <aliceduren01@byui.edu>
2022-05-02 14:57:29 -0600

    yrahyeahhtea

---
+++ js practice/prace.js
@@ -0,0 +1,23 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+  alert('I am a placeholder');
+}
+let x = 5 + 11;
+let y = 20 % 6;
+// document.write(y);
+var a = 0;
+let b = 10;
+var a = ++b;
+document.write('<h1>Hello World!<h1>');
+console.log('Sup!');
