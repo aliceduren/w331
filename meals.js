ad9dbb3b8ff3b1434c15547a2f0b2ffeb8785e66
aliceduren <aliceduren01@byui.edu>
2022-05-17 11:09:31 -0600

    yes

--- meals.js
+++ meals.js
@@ -1,4 +1,9 @@
 const requestURL = 'https://www.themealdb.com/api/json/v1/1/random.php';
+function reload() {
+  // location.reload();
+  document.querySelector('button').textContent =
+    'Lol. Just hit the reload button, stupid.';
+}
 
 fetch(requestURL)
   .then(function (response) {
@@ -12,46 +17,44 @@ fetch(requestURL)
       let h2 = document.createElement('h2');
       h2.textContent =
         'Category: ' + meaLs[0].strArea + ' ' + meaLs[0].strCategory;
+
       let h3 = document.createElement('h3');
       h3.textContent = 'Meal: ' + meaLs[0].strMeal;
-      let a_h3 = document.createElement('p');
-      a_h3.textContent = 'Instructions: ' + meaLs[0].strInstructions;
+
       let image = document.createElement('img');
       image.setAttribute('src', meaLs[0].strMealThumb);
       image.alt = meaLs[0].strMeal;
-
+      let adobtP = document.createElement('p');
+      let a = document.createElement('a');
+      var linkText = document.createTextNode('Recipe Source');
+      a.appendChild(linkText);
+      a.title = 'my title text';
+      a.href = meaLs[0].strSource;
+      adobtP.appendChild(a);
+      console.log(meaLs[0]);
       //omfg
       let ingrTit = document.createElement('h2');
       ingrTit.textContent = 'Ingredients:';
       let ingre = document.createElement('p');
-      let dishTit = document.createElement('h2');
-      dishTit.textContent = 'Tools Needed:';
-      let dish = document.createElement('p');
       let tryAgain = Object.values(meaLs[0]);
-      let hmmMMM;
-      let hMMMMM;
       console.log(tryAgain);
-      for (let f = 9; f < 28; f++) {
+      for (let f = 9, c = 29; f < 28, c < 43; f++, c++) {
         //should be 28 but okay
-        hmmMMM = tryAgain[f];
 
         tryAgain.filter((n) => n);
-        ingre.innerHTML += hmmMMM + '<br>';
+        ingre.innerHTML += tryAgain[c] + ' ' + tryAgain[f] + '<br>';
       }
-      for (let f = 29; f < 43; f++) {
-        //should be 28 but okay
-        hmmMMM = tryAgain[f];
-        dish.innerHTML += hmmMMM + '<br>';
-        console.log(hmmMMM);
-      }
+      let a_h3 = document.createElement('p');
+      a_h3.textContent = 'Instructions: ' + meaLs[0].strInstructions;
+
       card.appendChild(h2);
       card.appendChild(h3);
-      card.appendChild(a_h3);
+
       card.appendChild(image);
+      card.appendChild(adobtP);
       card.appendChild(ingrTit);
-      card.appendChild(ingre);
-      card.appendChild(dishTit);
-      card.appendChild(dish);
+      card.appendChild(ingre).style.textTransform = 'lowercase';
+      card.appendChild(a_h3);
       document.querySelector('.tester').appendChild(card);
     }
   });
